cd1d7b6f492004d23579a7726fc4efaebc779285
BlueNebulaa <nbil.ibrahim04@gmail.com>
2025-12-24 12:41:28 +0700

    fixing workflows

--- preprocessing/automate_Muhammad-Nabil-Ibrahim.py
+++ preprocessing/automate_Muhammad-Nabil-Ibrahim.py
@@ -42,11 +42,11 @@ def save_data(df, output_path):
     df.to_csv(output_path, index=False)
 
 def run():
-    df = load_data("../heart-dissease-dataset/Heart_Disease_Prediction.csv")
+    df = load_data("./heart-dissease-dataset/Heart_Disease_Prediction.csv")
     df_clean = preprocess_data(df)
     save_data(
         df_clean,
-        "Eksperimen_SML_Muhammad-nabil-ibrahim/preprocessing/data_clean.csv"
+        "preprocessing/data_clean.csv"
     )
 
 if __name__ == "__main__":

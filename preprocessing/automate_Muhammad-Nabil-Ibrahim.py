9cc7032ca71686155f840f90e2db49416eebc018
BlueNebulaa <nbil.ibrahim04@gmail.com>
2025-12-24 12:51:00 +0700

    fixing path2

--- preprocessing/automate_Muhammad-Nabil-Ibrahim.py
+++ preprocessing/automate_Muhammad-Nabil-Ibrahim.py
@@ -46,7 +46,7 @@ def run():
     df_clean = preprocess_data(df)
     save_data(
         df_clean,
-        "namadataset_preprocessing/data_clean.csv"
+        "data_clean.csv"
     )
 
 if __name__ == "__main__":
